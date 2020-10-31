c678d7a7bee4770972f39d3eb62034eb53217ad3
Federico Castañares <federico.castanares@gmail.com>
2020-10-31 19:17:07 -0300

    using axiosInstance in login

--- src/requests/login.js
+++ src/requests/login.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import {axiosApiInstance} from './instance';
 
 import {authenticateUser, dataUser} from './auth';
 
 export const loginRequest = async (user, password) => {
     try {
         var data = JSON.stringify({"email": user,"password" : password});
-        const response = await axios.post(`/login`, data, 
+        const response = await axiosApiInstance.post(`/login`, data, 
         {headers: {'Content-Type': 'application/json'}})
         authenticateUser(response.data.user.token);
         dataUser(response.data.user.name);

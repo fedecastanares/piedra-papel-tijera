7c06d42e114787e038ce144d221c9f518ac49a94
Federico Castañares <federico.castanares@gmail.com>
2021-01-18 13:10:38 -0300

    timeout lower

--- src/requests/instance.js
+++ src/requests/instance.js
@@ -3,7 +3,7 @@ import {getToken, deauthenticateUser} from './auth';
 
 // `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_BASE_URL}`
 
-export const axiosApiInstance = axios.create({baseURL: `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_BASE_URL}`, timeout: 2500});
+export const axiosApiInstance = axios.create({baseURL: `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_BASE_URL}`, timeout: 1500});
 
 axiosApiInstance.interceptors.request.use(
   async config => {
